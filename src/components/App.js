3fdbd791b9171a6076b3f6412c3fefcb55fcf286
Hrushi1509 <90896961+Hrushi1509@users.noreply.github.com>
2022-06-12 22:42:36 +0530

    rushi

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import { useState } from "react";
 const App = () => {
-
-
+  const [countchar, setcountchar] = useState(0);
+  const [countword, setcountword] = useState(0);
+  const font = { fontSize: "16px" };
+  const [range1, setrange1] = useState(font);
   return (
-    <div id="main">
-    </div>
-  )
-}
-
+    <>
+      Font size picker
+      <input
+        type="range"
+        min="16px"
+        max="32px"
+        onclick={function (event) {
+          setrange1(event.target.value);
+        }}
+      />
+      <input type="text" id="char-limit-input" value="50" />
+      <div>
+        <textarea
+          onChange={function display(event) {
+            setcountchar(event.target.value.length);
+            setcountword(event.target.value.split(" ").length);
+          }}
+          style={{ fontSize: { range1 } }}
+        ></textarea>
+      </div>
+      <div id="word-counter">Total number ofwords written {countword}</div>
+      <div id="char-counter">Total number of characters used {countchar}</div>
+    </>
+  );
+};
 
 export default App;
